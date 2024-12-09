3aac7c3915bd289d9f8d4af3a48e6bbf32f42776
thanya2003 <thanyashettigar@gmail.com>
2024-12-09 23:29:05 +0530

    card component

--- Card-com/src/Cards.jsx
+++ Card-com/src/Cards.jsx
@@ -2,7 +2,7 @@ import pic from "./assets/edit.png"
 function Cards(){
 return(
     <div className="Card">
-        <img src={pic} alt="profile pic" />
+        <img className="cardimg" src={pic} alt="profile pic"/>
         <h1>Thanya Shettigar</h1>
         <p>I an developing this site</p>
     </div>
